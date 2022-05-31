529abe1340680f5cc505d7872d82496a1c88db4e
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2022-05-31 12:06:49 +0200

    Card flip function implemented

--- components/Card.js
+++ components/Card.js
@@ -1,60 +1,127 @@
 import {
-  StyleSheet, Text, SafeAreaView,
-  TouchableOpacity, Image, View, Pressable
+  StyleSheet, Text, Image,
+  TouchableOpacity, View,
+  Animated, Pressable
 } from "react-native";
+import AnimatedAddition from "react-native-web/dist/vendor/react-native/Animated/nodes/AnimatedAddition";
+import {Component} from "react";
 
+class Card extends Component{
 
-const Card = ({data}) => {
-  return (
-    <TouchableOpacity
-      style={styles.card}
-      onPress={() => console.log(data)}>
-      <View>
-        <View style={{width: '100%', height: 250}}>
-          <Image
-            style={styles.image}
-            resizeMode='cover'
-            source={{
-              uri: data.download_url,
-            }}/>
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0);
+    this.value = 0;
+    this.animatedValue.addListener(({value}) => {
+      this.value = value;
+    })
+
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg'],
+    })
+
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg'],
+    })
+
+  }
+
+  flipCard = () => {
+    if (this.value >= 90){
+      Animated.spring(this.animatedValue,{
+        toValue: 0,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true
+      }).start();
+    } else {
+      Animated.spring(this.animatedValue,{
+        toValue: 180,
+        friction: 8,
+        tension: 10,
+        useNativeDriver: true
+      }).start();
+    }
+  }
+
+ render() {
+   const frontAnimatedStyle = {
+     transform: [
+       { rotateY: this.frontInterpolate}
+     ]
+   }
+
+   const backAnimatedStyle ={
+     transform: [
+       { rotateY: this.backInterpolate}
+     ]
+   }
+    return (
+      <Pressable
+        style={styles.container}
+        onPress={() => this.flipCard()}>
+        <View>
+          <Animated.View style={[styles.card, frontAnimatedStyle]}>
+              <Image
+                style={styles.image}
+                resizeMode='cover'
+                source={{
+                  uri: this.props.data.download_url,
+                }}/>
+            <Text
+              style={styles.author}>
+              {this.props.data.author}
+            </Text>
+          </Animated.View>
+
+          <Animated.View style={[styles.card, styles.back, backAnimatedStyle]}>
+            <Text
+              style={styles.author}>
+              {this.props.data.author}
+            </Text>
+          </Animated.View>
         </View>
-        <Text
-          style={styles.author}>
-          {data.author}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
+      </Pressable>
+    );
+ }
 }
 export default Card;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   card: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderRadius: 8,
+    width: 400,
+    height: 300,
     margin: 10,
     marginBottom: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#fff',
+    backfaceVisibility: "hidden",
   },
   image: {
+    flex: 9,
     width: '100%',
-    height: '100%',
+    height: 250,
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
   },
   author: {
     flex: 1,
     fontSize: 25,
-    alignSelf: 'center',
     padding: 5,
   },
+  back: {
+    position: "absolute",
+  },
+
 });
 
